c9cbbe95bf8b5a29426750c7abcc7ec22c422faa
Flurry58 <51930471+Flurry58@users.noreply.github.com>
2021-06-02 09:42:49 -0400

    Update server.py

--- server.py
+++ server.py
@@ -7,6 +7,21 @@ app = Flask(__name__)
 def main():
   return render_template('index.html')
 
+@app.route('/query-example')
+def query_example():
+    # if key doesn't exist, returns None
+    language = request.args.get('language')
+
+    # if key doesn't exist, returns a 400, bad request error
+    framework = request.args['framework']
+
+    # if key doesn't exist, returns None
+    website = request.args.get('website')
+
+    return '''
+              <h1>The language value is: {}</h1>
+              <h1>The framework value is: {}</h1>
+              <h1>The website value is: {}'''.format(language, framework, website)
 
 def run():
   app.run(host='0.0.0.0', port=8080)
