601c9aef3ee5c6aea1724585123d8282a09fc959
Flurry58 <51930471+Flurry58@users.noreply.github.com>
2021-06-02 09:50:22 -0400

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, url_for, render_template
 from threading import Thread
 
 app = Flask(__name__)
-
+result = ""
 @app.route('/')
 def main():
   return render_template('index.html')
@@ -18,11 +18,7 @@ def query_example():
     # if key doesn't exist, returns None
     website = request.args.get('website')
 
-    return '''
-              <h1>The language value is: {}</h1>
-              <h1>The framework value is: {}</h1>
-              <h1>The website value is: {}'''.format(language, framework, website)
-
+    result = language
 def run():
   app.run(host='0.0.0.0', port=8080)
 
